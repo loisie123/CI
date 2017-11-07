f23693bf3245472643db07962974dd54c7c6d5c3
koenderks <koen-derks@hotmail.com>
2017-11-07 23:12:52 +0100

    worked out network, need to discuss the inputs!

--- neural_network.py
+++ neural_network.py
@@ -40,22 +40,22 @@ np.random.seed(1)
 # initialize weights randomly
 syn0 = np.random.random(21)
 
-for iter in range(1,10):
+# Lenght of data is 4360
+for iter in range(0,len(in_data)-2):
 
     # forward propagation
     l0 = np.asarray(in_data[iter])
-    l1 = nonlin(np.dot(l0,syn0))
+    l1 = nonlin(l0*syn0)
 
     #how much did we miss?
-    l1_error = out_data - l1
+    l1_error = in_data[iter + 1] - l1
 
     #multiply how much we missed by the
     #slope of the sigmoid at the values in l1
     l1_delta = l1_error * nonlin(l1,True)
 
     #update weights
-    print(l0.T, l1_delta)
-    syn0 += np.dot(l0.T,l1_delta)
+    syn0 += np.dot(l0,l1_delta)
 
 print("Output After Training:")
 print(l1)
