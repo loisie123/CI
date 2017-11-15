28b2141e028be8ec99287ab3e2dfe7160a861b9a
Lois van Vliet <l.vanvliet@p-x.systems>
2017-11-15 12:26:34 +0100

    my_driver.py

--- my_driver.py
+++ my_driver.py
@@ -1,20 +1,70 @@
 # from pytocl.driver import Driver
 # from pytocl.car import State, Command
 from neural_try import *
+import csv
+import numpy
 
-
-#
 # class MyDriver(Driver):
-#     print("hello")
-#     # Override the `drive` method to create your own driver
+#     """
+#     """
 #
-#     # def drive(self, carstate: State) -> Command:
-#     #     # Interesting stuff
-#     #     command = Command(...)
-#     #     return command
-
-
-
+#     def __init__(self, logdata =True):
+#         self.steering_ctrl = CompositeController(
+#             ProportionalController(0.4),
+#             IntegrationController(0.2, integral_limit=1.5),
+#             DerivativeController(2)
+#         )
+#         self.acceleration_ctrl = CompositeController(
+#             ProportionalController(3.7),
+#         )
+#         self.data_logger = DataLogWriter() if logdata else None
+#
+#
+#     def drive(self, carstate: State):
+#
+#         """
+#         Produces driving command in response to newly received car state.
+#
+#         This is a dummy driving routine, very dumb and not really considering a
+#         lot of inputs. But it will get the car (if not disturbed by other
+#         drivers) successfully driven along the race track.
+#         """
+#
+#         command = Command()
+#
+#         self.steer(carstate, 0.0, command)
+#
+#         # ACC_LATERAL_MAX = 6400 * 5
+#         # v_x = min(80, math.sqrt(ACC_LATERAL_MAX / abs(command.steering)))
+#         v_x = 80
+#
+#         self.accelerate(carstate, v_x, command)
+#
+#         if self.data_logger:
+#             self.data_logger.log(carstate, command)
+#
+#         return command
+#
+#
+#     def train_NN(path_to_filename):
+#         w1, w2 = NN(path_to_filename)
+#         return w1, w2
+#
+#
+#     def create_ouput(input_line):
+#         """
+#         Function that creates output from an input_line
+#         """
+#         w1, w2 = train_NN('train_data/aalborg.csv')
+#         y_variable = torch.autograd.Variable(input_line, requires_grad=False)
+#         ipt = y_variable.view(1, 21)
+#         y_pred = ipt.mm(w1)
+#         out = y_pred.mm(w2)
+#         #output variables 0: acceleration  (has to be zero or 1)
+#
+#
+#         return out
+#
 
 
 
@@ -34,30 +84,37 @@ for row in X:
 firstline = torch.FloatTensor(in_data[0])
 
 # function that calls on the neural network
+
 def train_NN(path_to_filename):
     w1, w2 = NN(path_to_filename)
     return w1, w2
 
 def create_ouput(input_line):
-    """
-    Function that creates output from an input_line
-    """
-    w1, w2 = train_NN('train_data/aalborg.csv')
-    y_variable = torch.autograd.Variable(input_line, requires_grad=False)
-    ipt = y_variable.view(1, 21)
-    y_pred = ipt.mm(w1)
-    out = y_pred.mm(w2)
-    #output variables 0: acceleration  (has to be zero or 1)
-   
-
-    return out
+     """
+     Function that creates output from an input_line
+     """
+     w1, w2 = train_NN('train_data/aalborg.csv')
+     y_variable = torch.autograd.Variable(input_line, requires_grad=False)
+     ipt = y_variable.view(1, 21)
+     y_pred = ipt.mm(w1)
+     out = y_pred.mm(w2)
+     #output variables 0: acceleration  (has to be zero or 1
+     return out
 
 
 
-print("Hello ")
+output = create_ouput(firstline)
+accelarator = output.data[0,0]
+
+if accelarator > 0.5:
+    print(True)
 
 
 
-create_ouput(firstline)
 
-# input new data into the model and get output that calls on the accelaration methods.
+
+
+
+
+
+#input new data into the model and get output that calls on the accelaration methods.
