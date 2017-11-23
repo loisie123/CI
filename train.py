0c70b0c4eecfe270a639e660d586bd867598c714
Lois van Vliet <l.vanvliet@p-x.systems>
2017-11-23 13:28:26 +0100

    plan van aanpak

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ dtype = torch.FloatTensor
 # dtype = torch.cuda.FloatTensor # Uncomment this to run on GPU
 
 
-def open_file(path_to_filename, path_to_filename2, path_to_filename3)
+def open_file(path_to_filename, path_to_filename2, path_to_filename3):
     with open(path_to_filename) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
         X = []
@@ -46,50 +46,50 @@ def create_folds(X, k):
         folds.append([train_set, test_set])
     return folds
 
-                          
+
 def train(fold, H, lr, iterations):
-                          
+
     net = Net() # kloppen 3 en 23 ?
-                          
+
     train_data = fold[0]
     train_input = Variable(torch.FloatTensor(train_data[:][3:]).type(dtype), requires_grad=False)
     train_target = Variable(torch.FloatTensor(train_data[:][:3]).type(dtype), requires_grad=False)
-                                                    
+
     # try different loss functions
     loss_function = nn.MSELoss()
     # nn.CrossEntropyLoss()
-    
+
     # try different weight optimizers
     # for example: SGD, Nesterov-SGD, Adam, RMSProp, etc
     optimizer = torch.optim.SGD(net.parameters(), lr=lr)
-                          
+
     for i in range(iterations):
         # zero gradient buffers
         optimizer.zero_grad()
-                          
+
         # find output of network
         train_output = net(train_input)
-                          
+
         # error of output and target
         loss = loss_function(train_output, train_target)
-                          
-        # backpropagate the error                     
+
+        # backpropagate the error
         loss.backward()
-                          
+
         # update the weights
-        optimizer.step()        
-          
-    
+        optimizer.step()
+
+
     # find loss of test set
     test_data = fold[1]
     test_input = Variable(torch.FloatTensor(test_data[:][3:]).type(dtype), requires_grad=False)
     test_target = Variable(torch.FloatTensor(test_data[:][:3]).type(dtype), requires_grad=False)
     test_output = net(test_input)
     test_loss = loss_function(test_output, test_target)
-                          
-    return net.parameters(), test_loss                   
-                          
-                          
+
+    return net.parameters(), test_loss
+
+
 """
 parameters:
 path_to_filename
@@ -99,18 +99,20 @@ H: list of non-zero integers; the kth number corresponds with the number of node
 lr: learning rate (default is 1e-17)
 iterations: number of iterations (non-zero integer)
 k: number of folds (non-zero integer)
-"""                    
-def main(path_to_filename, path_to_filename2 = None, path_to_filename3  = None, H, lr = 1e-17, iterations, k):
-                          
+"""
+def main(H,iterations, k, path_to_filename, path_to_filename2 = None, path_to_filename3  = None,  lr = 1e-17):
+
     X = open_file(path_to_filename, path_to_filename2, path_to_filename3)
     folds = create_folds(X, k)
-                          
-    best_weights = None        
-    error = 10^40        
+
+    best_weights = None
+    error = 10^40
     for fold in folds:
         weights, test_loss = train(fold, H, lr, iterations)
         if error > test_loss:
             error = test_loss
             best_weights = weights
-                          
+
     return best_weights
+
+main(2, 10000, 5, '/Users/loisvanvliet/Documents/studie/2017:2018/Computational intelligence/CI/train_data/aalborg.csv',path_to_filename2 = '/Users/loisvanvliet/Documents/studie/2017:2018/Computational intelligence/CI/train_data/alpine-1.csv', path_to_filename3 = '//Users/loisvanvliet/Documents/studie/2017:2018/Computational intelligence/CI/train_data/f-speedway.csv' )
