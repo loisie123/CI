edbe1b1ed725eab18de83bd181348aef92836de8
Lois van Vliet <l.vanvliet@p-x.systems>
2017-11-23 14:00:28 +0100

    ea

--- ea.py
+++ ea.py
@@ -1,1 +1,24 @@
-ea.py
+def makepopulation(first = FALSE):
+    #amaakt de eerste populatie
+    return population
+
+def fitness():
+    return fitness
+
+def selectParents(lijst_met_fitnesvalues):
+    #pak de 5 beste en 3 random.
+    return beste, random
+
+def mutate(random):
+    #mutate de random selection
+    return network
+
+
+def breed(network1, network2):
+    return child1, child2
+
+
+
+def main():
+    #dit is de main function.
+    return new_popultion
