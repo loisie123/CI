c0fdb5ddc1c4fdc7c96cff1e2d5ce8515891ed73
koenderks <koen-derks@hotmail.com>
2017-11-24 15:46:30 +0100

    added selectParents function

--- ea.py
+++ ea.py
@@ -1,16 +1,36 @@
-def makepopulation(first = FALSE):
+import random
+import operator
+import numpy as np
+
+def makepopulation(first = False):
     #amaakt de eerste populatie
     return population
 
 def fitness():
     return fitness
 
-def selectParents(lijst_met_fitnesvalues):
-    #pak de 5 beste en 3 random.
-    return beste, random
+def selectParents(fitness = None):
+
+    ## Returns indexes in fitness list of selected parents (5 best and 3 random) ##
+
+    control = fitness[:]
+
+    index_beste = sorted(range(len(fitness)), key=lambda i: fitness[i])[-5:]
+
+    for index in sorted(index_beste, reverse=True):
+        del fitness[index]
+
+    random_selection = np.random.choice(fitness, 3, replace=False)
+    random_selection = list(random_selection)
+    index_random = []
+    for item in random_selection:
+        index_random.append(control.index(item))
+
+    return index_beste, index_random
 
 def mutate(random):
     #mutate de random selection
+
     return network
 
 
