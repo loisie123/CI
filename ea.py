c7264f4846b74c949ec3d7edc8dba24081df8df7
koenderks <koen-derks@hotmail.com>
2017-11-25 02:10:38 +0100

    added comments

--- ea.py
+++ ea.py
@@ -32,18 +32,18 @@ def selectParents(fitness = None):
     ## INPUT: list of fitness values of networks
     ## OUTPUT: index of 5 best networks in fitness-list, index of 3 random networks in fitness-list.
 
-    control = fitness[:]
+    control = fitness[:] # clone fitness for getting index of random values later on
 
-    index_beste = sorted(range(len(fitness)), key=lambda i: fitness[i])[-5:]
+    index_beste = sorted(range(len(fitness)), key=lambda i: fitness[i])[-5:] # Take 5 best
 
-    for index in sorted(index_beste, reverse=True):
+    for index in sorted(index_beste, reverse=True): # delete them from the fitness set
         del fitness[index]
 
-    random_selection = np.random.choice(fitness, 3, replace=False)
-    random_selection = list(random_selection)
+    random_selection = np.random.choice(fitness, 3, replace=False) # take 3 random from the remaining set
+    random_selection = list(random_selection)   # make it a list
     index_random = []
     for item in random_selection:
-        index_random.append(control.index(item))
+        index_random.append(control.index(item))    # get the index
 
     return index_beste, index_random
 
@@ -54,19 +54,19 @@ def mutate(weights_matrices):
 
     mutation = []
 
-    mutation_indicator = np.random.choice(2, 1, p=[.8, .2])
+    mutation_indicator = np.random.choice(2, 1, p=[.8, .2]) # mutation with probability of .2
 
-    if mutation_indicator == 1:
+    if mutation_indicator == 1: # if mutate
         print("MUTATION")
         for idx, mat in enumerate(weights_matrices):
-            mat = np.asarray(mat)
-            new = np.random.permutation(mat)
-            mutation.append(new)
+            mat = np.asarray(mat)   # make it array
+            new = np.random.permutation(mat)    # shuffle array
+            mutation.append(new)    # add shuffled matrix to list
 
-        return mutation
+        return mutation # return list of mutated weight matrices
 
-    else:
-        return weights_matrices
+    else: # if not mutate
+        return weights_matrices # return original network
 
 def breed(network1, network2):
 
@@ -74,18 +74,18 @@ def breed(network1, network2):
     ## OUTPUT: list of weights arrays of child network 1 and list of weights arrays of child network 2
 
     # Child 1
-    CH1 = []
-    for ind, mat in enumerate(network1):
-        child1 = np.zeros((mat.shape[0], mat.shape[1]))
-        for idx, row in enumerate(mat):
-            selection_indicator = np.random.choice(2, 1, p=[.8, .2]) # 0 for parent 1, 1 for parent 2
+    CH1 = []    # list to store child 1's weight matrices
+    for ind, mat in enumerate(network1):    # for every weight matrix
+        child1 = np.zeros((mat.shape[0], mat.shape[1])) # create zero matrix
+        for idx, row in enumerate(mat): # for every row in current weight matrix
+            selection_indicator = np.random.choice(2, 1, p=[.8, .2]) # choose parent (0 for parent 1, 1 for parent 2)
             if selection_indicator == 0:
-                child1[idx] = network1[ind][idx]
+                child1[idx] = network1[ind][idx] # take row from parent 1
             else:
-                child1[idx] = network2[ind][idx]
+                child1[idx] = network2[ind][idx] # take row from parent 2
         CH1.append(child1)
 
-    # Child 2
+    # Child 2, same principle
     CH2 = []
     for ind, mat in enumerate(network1):
         child2 = np.zeros((mat.shape[0], mat.shape[1]))
