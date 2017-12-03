0580d537f8ee39120e2521c876efcd00115df910
Lois van Vliet <l.vanvliet@p-x.systems>
2017-12-03 21:35:57 +0100

    os.py

---
+++ os.py
@@ -0,0 +1,15 @@
+import OS
+
+voor elke generatie:
+    voor elke populatie:
+
+        voor elk individu:
+            start de race:
+
+
+            sla de fitnesscore op
+
+
+import os
+
+os.system("command")
