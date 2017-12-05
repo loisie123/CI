aea130c2121ac88cc577bbc2105435d479581b07
Lois van Vliet <l.vanvliet@p-x.systems>
2017-12-05 21:39:32 +0100

    species

--- ea2.py
+++ ea2.py
@@ -11,17 +11,16 @@ from train import *
 
 def makepopulation(parents_file = None):
     if parents_file == None:
-        layer_info = [[58, 27, 36, 24, 3]]
+        layer_info = [[58, 39, 21, 6, 24, 3]]
         iterations = 1000
         lr = 45*10**-7
         populations = {}
+        family = []
         for elem in layer_info:
-            for i in range(20):
+            for i in range(2):
                 print(i)
-                family = []
                 net = create_nn(iterations, elem, '/Users/loisvanvliet/Documents/studie/2017:2018/Computational intelligence/CI/train_data/own_races.csv',path_to_filename2 = '/Users/loisvanvliet/Documents/studie/2017:2018/Computational intelligence/CI/train_data/alpine_own.csv', path_to_filename3 = '/Users/loisvanvliet/Documents/studie/2017:2018/Computational intelligence/CI/train_data/multiple_Races.csv', lr = lr)
                 family.append(net)
-
     else:
         family = torch.load(parents_file)
     return family
@@ -37,13 +36,15 @@ def makepopulation(parents_file = None):
 #torch.save(populations[5], 'species_5.pt')
 
 species_1 = torch.load('species_1.pt')
-print(species_1[0])
 print(len(species_1))
-population=[]
-for key, value in species_1.items():
-    population.append(value)
+#print(len(species_1))
+#population=[]
+#for key, value in species_1.items():
+#    population.append(value)
 
-torch.save(population, 'species_1.pt')
+#torch.save(population, 'species_1.pt')
+
+
 #
 # def makepopulation(generatie, parents_file = None):
 #     if parents_file == None:
